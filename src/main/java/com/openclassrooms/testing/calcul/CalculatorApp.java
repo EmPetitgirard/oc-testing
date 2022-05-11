5c877a0ea77253687331df9a4b7ab811c963c7df
EmPetitgirard <emmanuel.petitgirard@gmail.com>
2022-05-11 20:41:06 +0200

    adding comment

--- src/main/java/com/openclassrooms/testing/calcul/CalculatorApp.java
+++ src/main/java/com/openclassrooms/testing/calcul/CalculatorApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApp {
 
     public static void main(String[] args) {
+    	// commentaire pour lancer sonarcloud
         SpringApplication.run(CalculatorApp.class, args);
     }
 }
